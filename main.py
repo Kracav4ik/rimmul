e05178ca1fded5063c318d7d76b13843ed7af32b
Kracav4ik <dikama@yandex.ru>
2016-09-20 19:04:56 +0300

    Fixed strange

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import sys
 
 class RimMul:
-    def __init__(self, string):
-        self.string = string
+    def __init__(self, string="0"):
         self.map = {
             "0": 0,
             "I": 1,
@@ -12,72 +12,105 @@ class RimMul:
             "D": 500,
             "M": 1000
         }
+        self.check_strange(string)
+        self.string = string
         self.i = 0
 
     def __add__(self, other):
         num1 = self.to_num(self.string)
         num2 = self.to_num(other.string)
-        return RimMul(self.to_rim((num1 + num2) % 4000))
+        if num1 and num2:
+            return RimMul(self.to_rim((num1 + num2) % 4000))
+        else:
+            return RimMul()
 
     def __sub__(self, other):
         num1 = self.to_num(self.string)
         num2 = self.to_num(other.string)
-        return RimMul(self.to_rim(num1 - num2))
+        if num1 and num2:
+            return RimMul(self.to_rim(num1 - num2))
+        else:
+            return RimMul()
 
     def __truediv__(self, other):
         num1 = self.to_num(self.string)
         num2 = self.to_num(other.string)
-        return RimMul(self.to_rim(num1 // num2))
+        if num1 and num2:
+            return RimMul(self.to_rim(num1 // num2))
+        else:
+            return RimMul()
 
     def __mul__(self, other):
         num1 = self.to_num(self.string)
         num2 = self.to_num(other.string)
-        return RimMul(self.to_rim((num1 * num2) % 4000))
+        if num1 and num2:
+            return RimMul(self.to_rim((num1 * num2) % 4000))
+        else:
+            return RimMul()
 
     def print(self):
-        print(self.string)
+        if self.string:
+            print(self.string)
+        else:
+            print("Your number incorrect\n")
+            sys.exit()
 
     def to_num(self, string):
         """
         :type string: str
         """
+        if not string:
+            print("Fuck you no\n")
+            sys.exit()
         result = 0
-        j = len(string)
-        i = 0
-        uncorrect = 0
-        while i < j:
-            if uncorrect >= 3:
+        size = len(string)
+        index = 0
+        incorrect = 0
+        self.check_strange(string)
+
+        while index < size:
+            if incorrect >= 3:
                 print("Fuck you no\n")
-                return
-            first_num = self.map.get(string[i])
-            if i != j - 1:
-                second_num = self.map.get(string[i + 1])
+                sys.exit()
+            first_num = self.map.get(string[index])
+            if index != size - 1:
+                second_num = self.map.get(string[index + 1])
                 if first_num < second_num:
-                    if i > 0:
-                        if first_num == self.map.get(string[i - 1]):
+                    if index > 0:
+                        if first_num == self.map.get(string[index - 1]):
                             print("Fuck you no\n")
-                            return
+                            sys.exit()
+                    if second_num == 2 * first_num:
+                        print("Fuck you no\n")
+                        sys.exit()
+                    if first_num == 5 or first_num == 50 or first_num == 500:
+                        print("Fuck you no\n")
+                        sys.exit()
                     result += second_num - first_num
-                    i += 2
+                    index += 2
                     continue
                 if first_num == second_num:
-                    uncorrect += 1
+                    if first_num == 5 or first_num == 50 or first_num == 500:
+                        print("Fuck you no\n")
+                        sys.exit()
+                    incorrect += 1
                 else:
-                    uncorrect = 0
+                    incorrect = 0
             result += first_num
-            i += 1
+            index += 1
         if result > 3999:
-            print("Fuck you no\n")
-            return
+            result %= 4000
         return result
 
     def to_rim(self, inti):
         """
         :type inti: int
         """
-        if inti > 3999:
+        if inti is None:
             print("Fuck you no\n")
-            return
+            sys.exit()
+        if inti > 3999:
+            inti %= 4000
         result = ""
         inti1k = inti // 1000
         if inti1k:
@@ -122,8 +155,26 @@ class RimMul:
         elif inti:
             for _ in range(inti):
                 result += "I"
-        else:
-            result += "0"
         return result
 
-
+    def check_strange(self, string):
+        for idx in range(3, len(string)):
+            rim_num = ""
+            if idx == 3:
+                rim_num = "I"
+            elif idx == 4:
+                rim_num = "V"
+            elif idx == 7:
+                rim_num = "X"
+            elif idx == 8:
+                rim_num = "L"
+            elif idx == 11:
+                rim_num = "C"
+            elif idx == 12:
+                rim_num = "D"
+            elif idx == 15:
+                rim_num = "M"
+            for corrix in range(0, idx):
+                if string[corrix] == rim_num:
+                    print("Fuck you no\n")
+                    sys.exit()
